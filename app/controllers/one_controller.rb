366da4d1fd51860e69a520e7ab534ee745f7d9a6
Vicals <vicals@likelion.org>
2018-07-11 12:46:22 +0900

    carrierwave

--- app/controllers/one_controller.rb
+++ app/controllers/one_controller.rb
@@ -1,15 +1,19 @@
 class OneController < ApplicationController
   before_action :authenticate_user!, except: :index
   def index
-    @post = One.all
+    @post = One.all.reverse
   end
 
   def create
+    uploader = ImageUploader.new
+    uploader.store!(params[:image])
+
     @post = One.new
     @post.title = params[:title]
     @post.content = params[:content]
     @post.category = params[:category]
     @post.user_id = current_user.id
+    @post.image = uploader.url
     @post.save
 
     redirect_to "/one/index"
